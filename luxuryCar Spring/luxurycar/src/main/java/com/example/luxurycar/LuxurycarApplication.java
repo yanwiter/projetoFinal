aa943a2a0bba1417cdddc7c43567c34cf36d5d55
yanwiter <yanwiter@gmail.com>
2021-11-18 13:38:32 -0300

    criacao os projetos
    criação os projetos spring e angular

---
+++ luxuryCar Spring/luxurycar/src/main/java/com/example/luxurycar/LuxurycarApplication.java
@@ -0,0 +1,13 @@
+package com.example.luxurycar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuxurycarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuxurycarApplication.class, args);
+	}
+
+}
---
+++ luxuryCar Spring/luxurycar/src/test/java/com/example/luxurycar/LuxurycarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.luxurycar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuxurycarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
