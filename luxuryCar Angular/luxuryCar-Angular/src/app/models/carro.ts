ad44839d08cf1ba2ac82788f5a66989f26a74c46
yanwiter <yanwiter@gmail.com>
2021-11-23 13:53:21 -0300

    correções

--- luxuryCar Angular/luxuryCar-Angular/src/app/models/carro.ts
+++ luxuryCar Angular/luxuryCar-Angular/src/app/models/carro.ts
@@ -1,4 +1,16 @@
-export interface Carro{
-
-
+export interface Carro {
+  marca: string;
+  nomeVeiculo:string;
+  modeloVeiculo:string;
+  cor:string;
+  numPlaca:string;
+  numChassi:string;
+  numRenavam:string;
+  Combustivel:string;
+  valorCompra: BigInteger;
+  UF: ;
+  DataCompra: Date;
+  numPortas: ;
+  DescVeiculo:string;
+  Opcionais:string[];
 }
