c36b7e08ec7bd4c482e597aac316add8dc37bfc6
yanwiter <yanwiter@gmail.com>
2021-11-29 19:18:56 -0300

    update cadastrar

--- luxuryCar Angular/luxuryCar-Angular/src/app/components/cadastrar-cliente/cadastrar-cliente.component.ts
+++ luxuryCar Angular/luxuryCar-Angular/src/app/components/cadastrar-cliente/cadastrar-cliente.component.ts
@@ -11,45 +11,49 @@ import { Cliente } from './../../models/Cliente';
   styleUrls: ['./cadastrar-cliente.component.css'],
 })
 export class CadastrarClienteComponent implements OnInit {
-
   cliente: Cliente = {
-    id:         '',
-    nomeComp:   '',
-    sexo:       '',
-    estCivil:   '',
+    id: '',
+    nomeComp: '',
+    sexo: '',
+    estCivil: '',
     nascimento: '',
-    cpf:        '',
-    rg:         '',
-    telefone:   '',
-    cnh:        '',
-    email:      '',
-    endereco:   ''
+    cpf: '',
+    rg: '',
+    telefone: '',
+    cnh: '',
+    email: '',
+    endereco: '',
   };
 
-  // nomeComp: FormControl = new FormControl(null, Validators.required);
-  // sexo: FormControl = new FormControl(null);
-  // estCivil: FormControl = new FormControl(null);
-  // nascimento: FormControl = new FormControl(null);
-  // cpf: FormControl = new FormControl(null, Validators.required);
-  // rg: FormControl = new FormControl(null, Validators.required);
-  // telefone: FormControl = new FormControl(null, Validators.pattern('[9-9999-9999]'));
-  // cnh: FormControl = new FormControl(null, Validators.required);
-  // email: FormControl = new FormControl(null, Validators.email);
-  // endereco: FormControl = new FormControl(null);
+  nomeComp: FormControl = new FormControl(null, Validators.required);
+  sexo: FormControl = new FormControl(null);
+  estCivil: FormControl = new FormControl(null);
+  nascimento: FormControl = new FormControl(null);
+  cpf: FormControl = new FormControl(null, Validators.required);
+  rg: FormControl = new FormControl(null, Validators.required);
+  cnh: FormControl = new FormControl(null, Validators.required);
+  telefone: FormControl = new FormControl(
+    null,
+    Validators.pattern('[(00) 00000-0000]')
+  );
+  email: FormControl = new FormControl(null, Validators.email);
+  endereco: FormControl = new FormControl(null);
 
   constructor(
     private service: ClienteService,
-    private toast:    ToastrService,
-    private router:          Router,
+    private toast: ToastrService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {}
 
   create(): void {
-    this.service.create(this.cliente).subscribe(() => {
+    this.service.create(this.cliente).subscribe(
+      () => {
         this.toast.success('Cliente cadastrado com sucesso', 'Cadastro');
         this.router.navigate(['clienteList']);
-      }, ex => {
+      },
+      (ex) => {
         console.log(ex);
         if (ex.error.errors) {
           ex.error.errors.forEach((element: any) => {
@@ -58,7 +62,17 @@ export class CadastrarClienteComponent implements OnInit {
         } else {
           this.toast.error(ex.error.message);
         }
-      })
+      }
+    );
   }
 
+  validaCampos(): boolean {
+    return (
+      this.nomeComp.valid &&
+      this.rg.valid &&
+      this.cpf.valid &&
+      this.cnh.valid &&
+      this.email.valid
+    );
+  }
 }
